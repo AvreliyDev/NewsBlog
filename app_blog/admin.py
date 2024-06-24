e336ee54ae639abb3dd4f3377e4f32a0addc994f
LionsCliff <avreliy.dev@gmail.com>
2024-06-24 16:37:56 +0300

    застосували ORM для статтей та категорій блога.

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -1,3 +1,47 @@
-from django.contrib import admin
+from django.contrib import admin 
+from django.shortcuts import get_object_or_404
+from .models import Article, ArticleImage, Category
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('category', 'slug')
+  prepopulated_fields = {'slug': ('category',)}
+  fieldsets = (
+    ('', {
+      'fields': ('category', 'slug'),
+    }),
+  )
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ArticleImageInline(admin.TabularInline):
+  model = ArticleImage
+  extra = 0
+  fieldsets = (
+    ('', {
+      'fields': ('title', 'image'),
+    }),
+  )
+
+
+class ArticleAdmin(admin.ModelAdmin):
+  # columns to display for the list of articles in the admin panel
+  list_display = ('title', 'pub_date', 'slug', 'main_page', 'category')
+  fieldsets = (
+    ('', {
+      'fields': ('pub_date', 'title', 'description', 'category', 'main_page'),
+    }),
+    ('Додатково', {
+      'fields': ('slug',),
+    }),
+  )
+  prepopulated_fields = {'slug': ('title',)}
+  inlines = [ArticleImageInline]
+
+  def delete_file(self, pk, request):
+    '''Delete an image.'''
+    obj = get_object_or_404(ArticleImage, pk=pk)
+    return obj.delete()
+
+admin.site.register(Article, ArticleAdmin)
